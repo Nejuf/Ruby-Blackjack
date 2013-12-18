fc78b0cfd0684b5d913ab3c0e86567ce8fbd9f58
Nejuf <nejuf1@gmail.com>
2013-12-17 17:40:18 -0800

    Adds Hand points calculation

--- spec/Hand_spec.rb
+++ spec/Hand_spec.rb
@@ -13,15 +13,17 @@ describe Hand do
 
 	end
 
+	before(:each) do
+		@c1 = double('Card', :rank => 1, :suit => 'DIAMONDS')
+		@c2 = double('Card', :rank => 3, :suit => 'HEARTS')
+		@c3 = double('Card', :rank => 13, :suit => 'CLUBS')
+		@c4 = double('Card', :rank => 8, :suit => 'SPADES')
+		@c5 = double('Card', :rank => 5, :suit => 'SPADES')
+	end
+
 	context '#add' do
 		before(:all) do
 			@hand = Hand.new
-		end
-
-		before(:each) do
-			@c1 = double('Card', :rank => 1, :suit => 'DIAMONDS')
-			@c2 = double('Card', :rank => 3, :suit => 'HEARTS')
-			@c3 = double('Card', :rank => 13, :suit => 'CLUBS')
 		end
 
 		it 'adds a card' do
@@ -32,13 +34,49 @@ describe Hand do
 
 		it 'adds multiple cards' do
 			@hand = Hand.new
-			@hand.add(*[@c2, @c3])
+			@hand.add(@c2, @c3)
 			expect(@hand.cards.length).to eq(2)
 		end
 	end
 
 	context '#split' do
 
+	end
+
+	context '#points' do
+		before(:each) do
+			@hand = Hand.new
+		end
+
+		it 'considers face cards 10 points' do
+			@hand.add(@c2, @c3)
+			expect(@hand.points).to eq(13)
+		end
+
+		it 'identifies bust' do
+			@hand.add(@c3, @c5)
+			expect(@hand.bust?).to be false
+			@hand.add(@c4)
+			expect(@hand.bust?).to be true
+		end
+
+		it 'identifies blackjack' do
+			expect(@hand.blackjack?).to be false
+			@hand.add(@c1, @c3)
+			expect(@hand.blackjack?).to be true
+		end
+
+		it 'does not treat more than 2 cards as blackjack' do
+			@hand.add(@c2, @c3, @c4)
+			expect(@hand.blackjack?).to be false
+		end
+
+		it 'treats Ace as 1 when it would otherwise bust' do
+			@hand.add(@c1, @c2)
+			expect(@hand.points).to eq(14)
+			@hand.add(@c3)
+			expect(@hand.points).to eq(14)
+		end
 	end
 
 end
--- src/Hand.rb
+++ src/Hand.rb
@@ -8,4 +8,41 @@ class Hand
 			cards << card
 		end
 	end
+
+	def points
+		total = 0
+		aces = 0
+
+		cards.each do |card|
+			if card.rank == 1
+				aces += 1
+			elsif card.rank > 10
+				total += 10
+			else
+				total += card.rank
+			end
+		end
+
+		while aces > 0 do
+			aces -= 1
+
+			if (total + 11 + aces) > 21
+				total += 1
+			else
+				total += 11
+			end
+		end
+
+		total
+	end
+
+	def bust?
+		points > 21
+	end
+
+	def blackjack?
+		return false if cards.length != 2
+		points == 21
+	end
+
 end
