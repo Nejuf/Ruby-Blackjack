0011ebf5fcf3c7b3f9db7351e19b6759f48d8b96
Nejuf <nejuf1@gmail.com>
2013-12-17 23:12:18 -0800

    Begins implementing actions

--- src/Game.rb
+++ src/Game.rb
@@ -31,8 +31,51 @@ class Game
 	end
 
 	def game_loop
-		until true == false
-			puts "loop"
+
+		while human_player_active
+			human_player_active = false
+			@players.each do |player|
+				player.hands.each do |hand|
+					if(hand.cards.length == 0)
+						hand.add(@deck.draw)
+					elsif(hand.cards.length == 1)
+						card = @deck.draw
+						card.show
+						hand.add(card)
+					else
+						player_turn = true
+						while player_turn
+							action = player.choose_play(@dealer_hand.points)
+
+							case action
+							when :bet
+
+							when :hit
+								new_card = @deck.draw
+								new_card.show
+								player.hand.add(new_card)
+							when :stay
+								player_turn = false
+							when :double_down
+							when :split
+							when :quit
+							else
+								raise "Unrecognized player action: #{action}"
+							end
+						end
+					end
+				end
+
+				if player.is_a?(HumanPlayer) && player.in_game?
+					human_player_active = true 
+				end
+			end
+		end
+
+		notify('Game Over!')
+		notify('Final totals:')
+		@players.each do |player|
+			notify("--#{player.name}: $#{@player.money}")
 		end
 	end
 end
